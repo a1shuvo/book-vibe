3478f9cbf4d83b8f85853ee9318b9b3db2b15c8b
a1shuvo <a1shuvosaha@gmail.com>
2025-04-16 21:58:27 +0600

    Create and organize folder structure and Create Basic NavBar and Footer

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {
-  createBrowserRouter,
   RouterProvider,
 } from "react-router";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello World Router</div>,
-  },
-]);
+import { router } from './routes/Routes.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
